c3ac13c27cb549ca8632f60e2d84f685d39f8abd
SAWADOGO Wendbe Norbert <wendbenorbertsawadogo@gmail.com>
2022-06-18 22:53:30 +0200

    first commit

---
+++ resources/views/accueil.blade.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>acceuil</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,600,600i,700,700i|Satisfy|Comic+Neue:300,300i,400,400i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="{{asset('css/style.css')}}" rel="stylesheet">
+<img src="{{asset('img/logo.png')}}" alt="">
+
+  <!-- =======================================================
+  * Template Name: Delicious - v4.7.1
+  * Template URL: https://bootstrapmade.com/delicious-free-restaurant-bootstrap-theme/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Top Bar ======= -->
+  <!-- <section id="topbar" class="d-flex align-items-center fixed-top topbar-transparent">
+   
+  </section> -->
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center header-transparent">
+    <!-- <div class="container-fluid container-xl d-flex align-items-center justify-content-between"> -->
+
+      <div class="logo me-auto">
+        <!-- Uncomment below if you prefer to use an image logo --> 
+         <a href="index.html"><img src="{{('asset/img/images3.jpg')}}" alt="" class=""></a>
+      </div>
+      
+      <h3 class="epi"><a href="index.html">UNIVERSITE NAZI BONI</a></h3>
+
+ 
+
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+    <div class="hero-container">
+      <!-- <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel"> -->
+
+        <!-- <ol class="carousel-indicators" id="hero-carousel-indicators"></ol> -->
+
+        <!-- <div class="carousel-inner" role="listbox"> -->
+
+          <!-- Slide 1 -->
+          <div class="{{asset('carousel-item active big-bg" style="background-image: url(asset/img/slide5.jpg); background-repeat: no-repeat;">
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <div>
+                  <div class= container>
+                  <div class="header">
+                             <a href="index.html"><img src="{{asset('/img/logo.png ')}}" alt="" class="img-fluid"></a>
+
+                  <h3 id="epihan">ADMINISTRATEUR</h3>
+                  </div>
+                  <div class="main">
+                 <form>
+                  <span>
+                  <i class="fa fa-user"></i>
+                  <input type="text" placeholder="Username"name="">
+                  </span>
+                  <span>
+                  <i class="fa fa_loc"> </i>
+                  <input type="email" placeholder="email" name="">
+                  </span>
+                  <span>
+                  <i class="fa fa_loc"></i>
+                  <input type="password" placeholder="password" name="">
+                  </span>
+                  <span>
+                  <i class="fa fa_loc"></i>
+                  <input type="password" placeholder="Cpassword" name="">
+                  </span>
+                  <button> Envoyer </button>
+                  </form>
+                 </div>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+    </div>
+    </div>
+    </section>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
